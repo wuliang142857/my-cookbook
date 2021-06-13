271b759e4f66f3067981f7aa586cd017f642e48d
wuliang142857 <wuliang142857@gmail.com>
2021-06-14 00:55:15 +0800

    修复备案问题

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <ParentLayout>
+        <template #page-bottom>
+            <div style="text-align: center;">
+                <a href="https://beian.miit.gov.cn">浙ICP备17055787号-1</a>
+            </div>
+        </template>
+    </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+
+export default {
+    name: 'Layout',
+    components: {
+        ParentLayout
+    }
+}
+</script>
