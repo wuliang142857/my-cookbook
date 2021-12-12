3358590508cfafe59a731eea7d39e959c2a6817c
wuliang142857 <wuliang142857@gmail.com>
2021-12-12 23:35:47 +0800

    使用vuepress-theme-hope来重构页面，之前遗留了部分配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,45 +1,29 @@
+const {config} = require("vuepress-theme-hope");
 const path = require("path");
 const os = require("os");
-const moment = require("moment");
-require("moment/locale/zh-cn");
-moment.locale(`zh-cn`);
+const navBarConfig = require("./navBar");
+const sideBarConfig = require("./sideBar");
 
-/**
- * 导航页
- */
-const NAV = [
-    {
-        text: "我的笔记",
-        link: "/cookbook/"
-    }
-];
+module.exports = config({
+    title: "我的个人知识库",
+    description: "学习、沉淀、分享 ……",
 
-const HEAD = [
-    ["link", {
-        rel: "icon",
-        href: "/wuliang142857.png"
-    }]
-];
-
-module.exports = ctx => ({
     dest: path.join(path.dirname(path.dirname(__dirname)), "dist"),
-    title: "GISTer's Cookbook",
-    description: `多年来一直没有很好的沉淀`,
+    temp: path.join(os.tmpdir(), "my-cookbook"),
     host: "0.0.0.0",
     port: "8080",
-    temp: path.join(os.tmpdir(), "my-cookbook"),
-    head: HEAD,
     cache: true,
-    markdown: {
-        lineNumbers: false,
-        extendMarkdown: md => {
-            md.use(require("markdown-it-mark"));
-            md.use(require("markdown-it-fontawesome"));
-        }
+
+    locales: {
+        "/": {lang: "zh-CN"},
     },
+
     themeConfig: {
-        nav: NAV,
-        sidebarDepth: 1,
+        logo: "/logo.svg",
+        hostname: "https://www.wuliang.me/cookbook/",
+        iconPrefix: 'icon-mycookbook-',
+
+        author: "wuliang142857",
         lastUpdated: "上次更新",
         repo: 'https://github.com/wuliang142857/my-cookbook', // 启用到 GitHub 仓库的链接，显示在页面右上角
         repoLabel: 'GitHub', // repo 链接显示的名字
@@ -47,45 +31,131 @@ module.exports = ctx => ({
         docsBranch: 'master', // 指向 GitHub 仓库的哪个分支
         editLinks: true, // 启用快速编辑的链接，显示在文章末尾的左下角
         editLinkText: "在 GitHub 上编辑此页",
-        smoothScroll: true
+
+        nav: navBarConfig,
+        sidebar: sideBarConfig,
+
+        search: true,
+        searchMaxSuggestions: 10,
+        searchPlaceholder: "Search...",
+
+        footer: {
+            display: true,
+            copyright: 'MIT Licensed | Copyright © 2019-present wuliang142857 | <a href="https://beian.miit.gov.cn">浙ICP备17055787号-1</a>',
+        },
+
+        comment: false,
+
+        copyright: false,
+
+        git: {
+            timezone: "Asia/Shanghai",
+        },
+
+        mdEnhance: {
+            enableAll: true,
+            presentation: {
+                plugins: ["highlight", "math", "search", "notes", "zoom"],
+            },
+        },
+
+        pwa: {
+            favicon: "/favicon.ico",
+            themeColor: "#46bd87",
+            cachePic: true,
+            apple: {
+                icon: "/assets/icon/apple-icon-152.png",
+                statusBarColor: "black",
+            },
+            msTile: {
+                image: "/assets/icon/ms-icon-144.png",
+                color: "#ffffff",
+            },
+            manifest: {
+                icons: [
+                    {
+                        src: "/assets/icon/chrome-mask-512.png",
+                        sizes: "512x512",
+                        purpose: "maskable",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/assets/icon/chrome-mask-192.png",
+                        sizes: "192x192",
+                        purpose: "maskable",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/assets/icon/chrome-512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/assets/icon/chrome-192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                ],
+                shortcuts: [
+                    {
+                        name: "Guide",
+                        short_name: "Guide",
+                        url: "/guide/",
+                        icons: [
+                            {
+                                src: "/assets/icon/guide-maskable.png",
+                                sizes: "192x192",
+                                purpose: "maskable",
+                                type: "image/png",
+                            },
+                            {
+                                src: "/assets/icon/guide-monochrome.png",
+                                sizes: "192x192",
+                                purpose: "monochrome",
+                                type: "image/png",
+                            },
+                        ],
+                    },
+                    {
+                        name: "Config",
+                        short_name: "Config",
+                        url: "/config/",
+                        icons: [
+                            {
+                                src: "/assets/icon/config-maskable.png",
+                                sizes: "192x192",
+                                purpose: "maskable",
+                                type: "image/png",
+                            },
+                            {
+                                src: "/assets/icon/config-monochrome.png",
+                                sizes: "192x192",
+                                purpose: "monochrome",
+                                type: "image/png",
+                            },
+                        ],
+                    },
+                    {
+                        name: "Basic",
+                        short_name: "Basic",
+                        url: "/basic/",
+                        icons: [
+                            {
+                                src: "/assets/icon/basic-maskable.png",
+                                sizes: "192x192",
+                                purpose: "maskable",
+                                type: "image/png",
+                            },
+                            {
+                                src: "/assets/icon/basic-monochrome.png",
+                                sizes: "192x192",
+                                purpose: "monochrome",
+                                type: "image/png",
+                            },
+                        ],
+                    },
+                ],
+            },
+        },
     },
-    plugins: [
-        ["autobar", {
-            stripNumbers: true,
-            maxLevel: 5,
-            pinyinNav: false,
-            setHomepage: "top",
-            multipleSideBar: true,
-            skipEmptyNavbar: true,
-            skipEmptySidebar: true,
-        }],
-        ["@vuepress/back-to-top", true],
-        // last-updated 插件
-        [
-            "@vuepress/last-updated",
-            {
-                transformer: (timestamp, lang) => {
-                    return moment(timestamp).fromNow();
-                }
-            }
-        ],
-        ["@vuepress/medium-zoom", true],
-        [
-            "@vuepress/search",
-            {
-                searchMaxSuggestions: 10
-            }
-        ],
-        ['@mr-hope/sitemap', {
-            hostname: "https://www.wuliang.me",
-            // 排除无实际内容的页面
-            exclude: ["/404.html"]
-        }
-        ],
-        ["vuepress-plugin-baidu-autopush", true],
-        ['permalink-pinyin', {
-            lowercase: false,
-            separator: '-'
-        }]
-    ]
 });
