ae9d446f6d6450007da159d05b552d15e86253fe
wuliang142857 <wuliang142857@gmail.com>
2021-12-29 19:09:37 +0800

    重新体用PWA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -59,7 +59,45 @@ module.exports = config({
             },
         },
 
-        pwa: false,
+        pwa: {
+            favicon: "/favicon.ico",
+            themeColor: "#46bd87",
+            cachePic: true,
+            apple: {
+                icon: "/assets/icon/apple-icon-152.png",
+                statusBarColor: "black",
+            },
+            msTile: {
+                image: "/assets/icon/ms-icon-144.png",
+                color: "#ffffff",
+            },
+            manifest: {
+                icons: [
+                    {
+                        src: "/assets/icon/chrome-mask-512.png",
+                        sizes: "512x512",
+                        purpose: "maskable",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/assets/icon/chrome-mask-192.png",
+                        sizes: "192x192",
+                        purpose: "maskable",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/assets/icon/chrome-512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/assets/icon/chrome-192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                ]
+            },
+        },
         
         sitemap: {
             changefreq: "always"
